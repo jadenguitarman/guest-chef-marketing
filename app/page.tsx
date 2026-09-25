c14a84c666429ee3b79685e589f6bfa7365d936b
Jaden Baptista <jadenguitarman@gmail.com>
2026-09-25 15:49:54 -0400

    Use supplied Portugal SVG artwork

--- app/page.tsx
+++ app/page.tsx
@@ -60,22 +60,16 @@ export default function Home() {
           </div>
 
           <div className="hero-art" aria-hidden="true">
-            <svg className="hero-art__map" viewBox="0 0 260 420" fill="none">
-              <title>Portugal, with Aveiro marked on the coast</title>
-              <g transform="translate(20 6) scale(0.78)">
-                <path
-                  className="hero-art__map-outline"
-                  d="M70 17c-13-2-24 5-33 15-9 9-12 20-9 33l16 62c3 12 2 22-1 35l-15 74c-3 15-12 26-20 38-6 10-9 26-11 43-2 15 2 25 12 34l14 17c7 8 15 11 28 10 8 0 11 4 12 15l2 31c1 15-5 31-8 43-3 11-1 17 9 18l19-1c14 0 27 6 39 8 12 2 24-3 34-8 12-6 16-13 15-25-2-12 2-24 12-35l21-22c9-10 13-19 8-28-5-9-13-15-19-24-7-10-8-17-1-29l20-36c6-11 7-19 1-28l-36-47c-8-10-8-18 1-23l18-8c10-5 14-14 15-26l-2-30c0-11 3-19 10-27l20-20c11-11 19-25 21-36 2-9-4-17-13-24l-6-18c-3-9-10-13-22-15l-26-2c-13-1-23 3-34 10-11 7-20 9-32 9l-25 2c-12 1-15-4-15-14v-15c0-10-5-15-15-15l-10 1Z"
-                />
-                <path className="hero-art__route" d="M76 128c42-18 85-14 125 5 20 10 34 24 45 41" />
-                <path className="hero-art__route hero-art__route--soft" d="M76 128c49 2 95 19 135 49 15 11 26 24 35 39" />
-                <circle className="hero-art__pin-ring" cx="76" cy="128" r="17" />
-                <circle className="hero-art__pin-ring hero-art__pin-ring--outer" cx="76" cy="128" r="27" />
-                <circle className="hero-art__pin" cx="76" cy="128" r="5" />
-                <path className="hero-art__sun" d="M187 71v-12M187 95V83M163 83h12M198 83h12M170 66l8 8M196 92l8 8M204 66l-8 8M178 92l-8 8" />
-                <text className="hero-art__label" x="96" y="125">AVEIRO</text>
-                <text className="hero-art__sub-label" x="96" y="137">where it begins</text>
-              </g>
+            <img className="hero-art__map-source" src="/portugal.svg" alt="" />
+            <svg className="hero-art__accents" viewBox="0 0 260 420" fill="none">
+              <path className="hero-art__route" d="M76 128c42-18 85-14 125 5 20 10 34 24 45 41" />
+              <path className="hero-art__route hero-art__route--soft" d="M76 128c49 2 95 19 135 49 15 11 26 24 35 39" />
+              <circle className="hero-art__pin-ring" cx="76" cy="128" r="17" />
+              <circle className="hero-art__pin-ring hero-art__pin-ring--outer" cx="76" cy="128" r="27" />
+              <circle className="hero-art__pin" cx="76" cy="128" r="5" />
+              <path className="hero-art__sun" d="M187 71v-12M187 95V83M163 83h12M198 83h12M170 66l8 8M196 92l8 8M204 66l-8 8M178 92l-8 8" />
+              <text className="hero-art__label" x="96" y="125">AVEIRO</text>
+              <text className="hero-art__sub-label" x="96" y="137">where it begins</text>
             </svg>
           </div>
 
